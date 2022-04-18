d44e75eed6b76ed4721dbf84a6dfe65855f11d2d
unknown <steinway.nathan@gmail.com>
2022-04-17 22:40:16 -0500

    more bug fixes

--- server/server.js
+++ server/server.js
@@ -13,6 +13,8 @@ const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    introspection: true,
+    playground: true,
     context: authMiddleware,
   });
 
@@ -28,6 +30,11 @@ startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
