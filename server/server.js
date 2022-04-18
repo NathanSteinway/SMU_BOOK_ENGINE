5ccb454ee6fe43c4d52474b99d86ba7b5365f684
unknown <steinway.nathan@gmail.com>
2022-04-17 21:14:01 -0500

    corrected server setup mistakes

--- server/server.js
+++ server/server.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
-
 const { typeDefs, resolvers } = require("./Schemas");
+const db = require('./config/connection');
 const { authMiddleware } = require("./utils/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
+const startServer = async () => {
 
-server.applyMiddleware({ app });
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+
+};
+
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
