42653ffcd73754eb8cf318364fa24da45947bed7
unknown <steinway.nathan@gmail.com>
2022-04-17 23:14:25 -0500

    last commit of the night

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection.js');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require("./Schemas");
+const db = require('./config/connection.js');
 const { authMiddleware } = require("./utils/auth");
 
 const app = express();
@@ -31,13 +30,10 @@ startServer();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
 
 
